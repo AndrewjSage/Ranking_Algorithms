93c8dc553e8a4d910c882fee70e5e3511cce2b72
alibek-m <alibek.mamyrbay@lawrence.edu>
2025-06-24 07:11:23 -0500

    Added two weeks of data to the .csv. Renamed a few files. Created a new file ranking_chess_code.R.

---
+++ Ali_work/ranking_chess_code.R
@@ -0,0 +1,9 @@
+# I will be doing most of the work in this file.
+# Current questions and brainstorm:
+# If I upload 52 weeks of data into the .csv, it will have about 400.000 observations.
+# That's a lot, but I don't think I will have issues with working 
+# with such a dataset.
+# Should I do filtering by ELO? It depends on the task I assign. 
+# What could be confounding variables?
+
+fide_games <- read.csv("fide_games_weekly.csv")
