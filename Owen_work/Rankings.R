d1eee59e20fea45422fc4798a79e188a491b0c84
Owen Horton <owen.n.horton@lawrence.edu>
2025-08-12 12:28:19 -0500

    Implemented a time-sensitive Bayesian model with the changes between periods being observed as a normal distribution instead of a t-distribution, the results are by far the best I have seen so far

--- Owen_work/Rankings.R
+++ Owen_work/Rankings.R
@@ -8,7 +8,7 @@ library(rjags)
 #source("weekly_res_gen.R")
 select <- dplyr::select
 
-# Setup NBA data
+# Setup Games data
 Games <- International.Soccer.Games.2018.2022
 Games <- Games %>% select(V2, V3, V4, V7, V9, V10)
 
@@ -58,17 +58,17 @@ Games$period[is.na(Games$period)] <- K + 1
 
 
 -------------------------------------------------------------------------------------
-  ## Bayesian constant strengths model
+  ## Bayesian adaptable strengths model
   -------------------------------------------------------------------------------------
 
 # Bayesian Model with jags
-  model <- "
+  model <- model <- "
 model {
   for (i in 1:n_games) {
     y[i] ~ dnorm(
-      strength[Home[i], period[i]] - 
-      strength[Away[i], period[i]] + 
-      alpha * h[i], 
+      strength[Home[i], period[i]] -
+      strength[Away[i], period[i]] +
+      alpha * h[i],
       prec_game
     )
   }
@@ -82,9 +82,12 @@ model {
   }
 
   alpha ~ dnorm(0, 0.01)
+
+  # Game-to-game variability
   sig_game ~ dt(0, 1, 1) T(0, )
   prec_game <- 1 / (sig_game * sig_game)
 
+  # Team strength evolution variability
   sig_team ~ dt(0, 1, 1) T(0, )
   prec_team <- 1 / (sig_team * sig_team)
 }
@@ -103,34 +106,44 @@ dat <- list(
   n_periods = K + 1
 )
 
+
 m <- jags.model(textConnection(model), dat, n.chains=3)   #run model
 parms <- c("strength", "alpha", "sig_game", "sig_team")
 r <- coda.samples(m, parms, n.iter=10000, n.burnin=100, thin=1)
 
 
-# look at the mean column
-# first two rows give estimates of alpha (home field parameter, and sigma_g, the est. game to game variability)
-# third row and below are the team strengths
-summary(r)[[1]]
 
+summary_df <- as.data.frame(summary(r)$statistics)
+summary_df$param <- rownames(summary_df)
 
-PQuant <- data.frame(summary(r)$quantiles)
+quant_df <- as.data.frame(summary(r)$quantiles)
+quant_df$param <- rownames(quant_df)
 
+strength_df <- summary_df %>%
+  inner_join(quant_df, by = "param") %>%
+  filter(grepl("^strength\\[", param)) %>%
+  mutate(
+    Team_index = as.integer(sub("strength\\[([0-9]+),([0-9]+)\\]", "\\1", param)),
+    Period = as.integer(sub("strength\\[([0-9]+),([0-9]+)\\]", "\\2", param)),
+    Team = all_teams[Team_index],
+    Mean = Mean,           # from summary stats
+    Median = `50%`,        # from quantiles
+    LCL = `2.5%`,
+    UCL = `97.5%`
+  ) %>%
+  select(Team_index, Period, Team, Mean, Median, LCL, UCL)
 
-srange <- 3:nrow(PQuant) # rows containing team strengths (they start in row 3)
+final_period <- max(strength_df$Period)
+final_rankings <- strength_df %>%
+  filter(Period == final_period) %>%
+  arrange(desc(Mean)) %>%
+  select(Team, Mean, Median, LCL, UCL)
 
+print(final_rankings)
 
+Graph <- ggplot(strength_df, aes(x = Period, y = Mean, color = Team)) +
+  geom_line() +
+  theme_minimal() +
+  labs(title = "Team Strength Over Time", y = "Strength", x = "Period")
 
-# collect in data frame
-n_teams <- length(Team)
-n_periods <- K + 1
-final_strength_indices <- ((n_periods - 1) * n_teams + 1):(n_periods * n_teams)
-
-Mean <- summary(r)[[1]][final_strength_indices, 1]
-Med  <- PQuant[final_strength_indices, 3]
-LCL  <- PQuant[final_strength_indices, 1]
-UCL  <- PQuant[final_strength_indices, 5]
-
-Rankings <- data.frame(Team, Mean, Med, LCL, UCL)
-Rankings <- Rankings |> arrange(desc(Mean))
-show(Rankings)
+show(Graph)
